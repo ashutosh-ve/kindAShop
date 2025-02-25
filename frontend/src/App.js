24cc3c606bf80283b38dc235290fc0d86db9786c
ashutoshve01 <axxuverma@gmail.com>
2025-02-25 12:15:15 +0530

    Header and footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import { Container } from 'react-bootstrap';
 import React from 'react'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export const App = () => {
   return (
-    <h1>Welcome to the KindAShop</h1>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To KindAShop</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
